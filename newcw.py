33336a02e80b6beb3cf983ee1fe696c6a916580a
DrDenwer <denwerdr77@gmail.com>
2021-07-02 19:56:27 +0300

    newcw
    newc

---
+++ newcw.py
@@ -0,0 +1,90 @@
+from telethon import *
+import random
+import logging
+from .. import loader, utils
+import asyncio
+
+logger = logging.getLogger(__name__)
+
+
+
+class YourMod(loader.Module):
+    """Description for module"""  # Translateable due to @loader.tds
+    strings = {"name": "mcw"}
+
+
+
+   
+    async def ncwcmd(self,event):
+        await event.delete()
+        
+        
+        
+     
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)  
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)
+          
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)  
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)
+          
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)  
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)
+          
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)  
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)  
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)  
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+        await asyncio.sleep(621)  
+
+        await event.respond("🕹 Действия")
+        await asyncio.sleep(1)  
+        await event.respond("👮 Патрулируем")
+
+
+        
